110e488aed00f399a49e4e88af9d5c167f4fb1ed
Stella <stella.millwood@gmail.com>
2026-01-30 17:49:51 +0100

    added more toggle buttons with images

--- src/app/projects/digi-post/digi-post.component.ts
+++ src/app/projects/digi-post/digi-post.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { PROJECTS, Project } from '../projects-data';
 import { ProjectInfoTableComponent } from "src/app/project-info-table/project-info-table.component";
+import { BeforeAfterComponent } from "src/app/before-after/before-after.component";
 
 @Component({
   selector: 'app-digi-post',
   standalone: true,
   templateUrl: './digi-post.component.html',
   styleUrl: './digi-post.component.css',
-  imports: [ProjectInfoTableComponent],
+  imports: [ProjectInfoTableComponent, BeforeAfterComponent],
 })
 export class DigiPostComponent {
   project: Project = PROJECTS.find(p => p.route === "/projects/digi-post")!;
