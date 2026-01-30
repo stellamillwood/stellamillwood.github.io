8b5037d4dade4eaee9c5becbffdcd19ab2da4bc9
Stella <stella.millwood@gmail.com>
2026-01-30 17:58:41 +0100

    added button toggle on digital stewardship

--- src/app/projects/digital_stewardship/digital_stewardship.component.ts
+++ src/app/projects/digital_stewardship/digital_stewardship.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { PROJECTS, Project } from '../projects-data';
 import { ProjectInfoTableComponent } from "src/app/project-info-table/project-info-table.component";
+import { BeforeAfterComponent } from "src/app/before-after/before-after.component";
 
 
 @Component({
@@ -8,7 +9,7 @@ import { ProjectInfoTableComponent } from "src/app/project-info-table/project-in
   standalone: true,
   templateUrl: './digital_stewardship.component.html',
   styleUrl: './digital_stewardship.component.css',
-  imports: [ProjectInfoTableComponent],
+  imports: [ProjectInfoTableComponent, BeforeAfterComponent],
 })
 export class DigitalStewardshipComponent {
   project: Project = PROJECTS.find(p => p.route === "/projects/digital-stewardship")!;
