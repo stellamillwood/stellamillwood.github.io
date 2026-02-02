72e08a66f000e30ca7e413cb529b0a7f00ec0b94
Stella <stella.millwood@gmail.com>
2026-02-02 11:39:30 +0100

    converted AF logos to svgs and changed the order of the cards

--- src/app/projects/projects-data.ts
+++ src/app/projects/projects-data.ts
@@ -12,7 +12,7 @@ export interface Project {
     {
       title: "Tajma",
       subtitle: "Time Reporting Tool",
-      image: "assets/tajma/tajma-logo.png",
+      image: "assets/tajma/tajma-logo.svg",
       description: "Tajma is a time-reporting tool for internal users at a government agency. It is built as a web application in Angular",
       route: "/projects/tajma", 
       tableData: [ 
@@ -21,28 +21,16 @@ export interface Project {
         { label: "My Role", value: "UX design and front-end development" } ]
     }, 
     {
-      title: "Stella Budget & Prognosis",
-      subtitle: "Redesign of Budget & Prognosis Web Application",
-      image: "assets/stella-budget-prognos/stella-budget-prognos-logo.png",
-      description: "The budget & prognosis application is used across organizational levels by business controllers, operations coordinators, and managers. I was tasked with redesigning the application’s UX. The system is developed externally, while I am responsible for UX research, design direction, and stakeholder alignment.",
-      route: "/projects/stella-budget-prognos", 
+      title: "Drop the Beat",
+      subtitle: "AR Music-Sharing",
+      image: "assets/drop-the-beat/drop-the-beat-logo.png",
+      description: "A playful concept that lets students “drop” music around campus for others to discover. The idea grew from exploring how hybrid study reduced spontaneous encounters at university, and how immersive tech could make campus feel more social again. Users could spot virtual speakers in AR, tap them to listen, watch them animate, and optionally connect with the creator through social platforms.",
+      route: "/projects/drop-the-beat", 
       tableData: [ 
-        { label: "Users", value: "Business controllers, operations coordinators and managers at a large government agency" },
-        { label: "My Role", value: "UX research, design direction, and stakeholder alignment" },
-        { label: "Constraints", value: "No recordings, domain complexity, external development" }, 
-        { label: "Duration", value: "6 months 2025-2026" }, 
-        { label: "Methods", value: "Exploratory interviews, user tests, affinity diagram, prioritization matrices" }]  
-    }, 
-    {
-      title: "DigiPost",
-      subtitle: "AI Streamlines Processing of Incoming Mail from Citizens",
-      image: "assets/digi-post/digi-post-logo.png",
-      description: "DigiPost",
-      route: "/projects/digi-post", 
-      tableData: [ 
-        { label: "Users", value: "Administrators at a large government agency" },
-        { label: "My Role", value: "UI designer" },
-        { label: "Duration", value: "2 days in 2025" } ]
+        { label: "Users", value: "Remote and in-person students at Stockholm University" },
+        { label: "Technologies and Tools", value: "Augmented Reality (AR), Unity and Figma" },
+        { label: "Duration", value: "2 months in 2021" }, 
+        { label: "My role", value: "UX and UI designer" }]
     },
     {
       title: "Digital Stewardship",
@@ -57,37 +45,29 @@ export interface Project {
         { label: "Methods", value: "Interviews, workshops, user journeys, wireframes and interactive prototypes" }]
     },
     {
-      title: "ACDC",
-      subtitle: "Augmenting Complex and Dynamic (ACDC) is an augmented reality (AR) project on digital twins.",
-      image: "assets/acdc/acdc-logo.png",
-      description: "In this project me and my team simulated people crossing an intersection in Kista Galleria. The simulation was created using Unity and Figma and could be interacted with using a tablet. By using a toolbar a user could, for example, pause the simulation and add more people to it. This project lasted 3 months in 2022 and was part of the course Design for Complex and Dynamic Contexts. ",
-      route: "/projects/acdc", 
-      hidden: true
+      title: "IMRS",
+      subtitle: "A Mixed Reality Experience for Comparing Input Methods",
+      image: "assets/imrs/imrs-logo.png",
+      description: "IMRS is a mixed reality tabletop experience developed in collaboration with Ericsson to explore how different input methods affect user interaction in MR. The project combined physical and virtual elements. With our Research-through-Design approach we collected user data to evaluate the methods' usability and inform design recommendations for smoother interactions in MR environments.",
+      route: "/projects/imrs", 
+      tableData: [ 
+        { label: "Technologies and Tools", value: "Mixed Reality, projection mapping, Unity, sensors, hand-tracking, physical props and Figma" },
+        { label: "My role", value: "User research and UI design" }, 
+        { label: "Duration", value: "3 months in 2022" }]
     },
     {
-      title: "AGoodSite",
-      subtitle: "Figma-based Design System for Museum Websites.",
-      image: "assets/agoodsite/agoodsite-logo.png",
-      description: "I completed a full-time design internship at a Stockholm-based design bureau. My main assignment was to create a web-based design system for the company’s clients (mainly museums) to ensure cohesive and efficient design processes across projects.",
-      route: "/projects/agoodsite", 
+      title: "Stella Budget & Prognosis",
+      subtitle: "Redesign of Budget & Prognosis Web Application",
+      image: "assets/stella-budget-prognos/stella-budget-prognos-logo.svg",
+      description: "The budget & prognosis application is used across organizational levels by business controllers, operations coordinators, and managers. I was tasked with redesigning the application’s UX. The system is developed externally, while I am responsible for UX research, design direction, and stakeholder alignment.",
+      route: "/projects/stella-budget-prognos", 
       tableData: [ 
-        { label: "Users", value: "Art Directors at the design bureau (directly) and their clients (indirectly)" },
-        { label: "Methods & Tools", value: "Figma (Auto Layout, variants, style guide and more) and competitive analysis" },
-        { label: "Duration", value: "6 months in 2021" }, 
-        { label: "My role", value: "UI designer" }]
-    },
-    {
-      title: "Drop the Beat",
-      subtitle: "AR Music-Sharing",
-      image: "assets/drop-the-beat/drop-the-beat-logo.png",
-      description: "A playful concept that lets students “drop” music around campus for others to discover. The idea grew from exploring how hybrid study reduced spontaneous encounters at university, and how immersive tech could make campus feel more social again. Users could spot virtual speakers in AR, tap them to listen, watch them animate, and optionally connect with the creator through social platforms.",
-      route: "/projects/drop-the-beat", 
-      tableData: [ 
-        { label: "Users", value: "Remote and in-person students at Stockholm University" },
-        { label: "Technologies and Tools", value: "Augmented Reality (AR), Unity and Figma" },
-        { label: "Duration", value: "2 months in 2021" }, 
-        { label: "My role", value: "UX and UI designer" }]
-    },
+        { label: "Users", value: "Business controllers, operations coordinators and managers at a large government agency" },
+        { label: "My Role", value: "UX research, design direction, and stakeholder alignment" },
+        { label: "Constraints", value: "No recordings, domain complexity, external development" }, 
+        { label: "Duration", value: "6 months 2025-2026" }, 
+        { label: "Methods", value: "Exploratory interviews, user tests, affinity diagram, prioritization matrices" }]  
+    }, 
     {
       title: "Flourish",
       subtitle: "A task-based mobile application for improving mental wellness using simulated AR for a growing plant.",
@@ -102,6 +82,25 @@ export interface Project {
         { label: "Methods", value: "user tests, workshops, empathy maps, brainstorming" }]
     },
     {
+      title: "DigiPost",
+      subtitle: "AI Streamlines Processing of Incoming Mail from Citizens",
+      image: "assets/digi-post/digi-post-logo.svg",
+      description: "DigiPost",
+      route: "/projects/digi-post", 
+      tableData: [ 
+        { label: "Users", value: "Administrators at a large government agency" },
+        { label: "My Role", value: "UI designer" },
+        { label: "Duration", value: "2 days in 2025" } ]
+    },
+    {
+      title: "ACDC",
+      subtitle: "Augmenting Complex and Dynamic (ACDC) is an augmented reality (AR) project on digital twins.",
+      image: "assets/acdc/acdc-logo.png",
+      description: "In this project me and my team simulated people crossing an intersection in Kista Galleria. The simulation was created using Unity and Figma and could be interacted with using a tablet. By using a toolbar a user could, for example, pause the simulation and add more people to it. This project lasted 3 months in 2022 and was part of the course Design for Complex and Dynamic Contexts. ",
+      route: "/projects/acdc", 
+      hidden: true
+    },
+    {
       title: "G-Force",
       subtitle: "A VR Experience Exploring Sensory Alignment with Tangible Objects",
       image: "assets/g-force/g-force-logo.png",
@@ -113,15 +112,16 @@ export interface Project {
         { label: "Duration", value: "3 months in 2022" }]
     },
     {
-      title: "IMRS",
-      subtitle: "A Mixed Reality Experience for Comparing Input Methods",
-      image: "assets/imrs/imrs-logo.png",
-      description: "IMRS is a mixed reality tabletop experience developed in collaboration with Ericsson to explore how different input methods affect user interaction in MR. The project combined physical and virtual elements. With our Research-through-Design approach we collected user data to evaluate the methods' usability and inform design recommendations for smoother interactions in MR environments.",
-      route: "/projects/imrs", 
+      title: "AGoodSite",
+      subtitle: "Figma-based Design System for Museum Websites.",
+      image: "assets/agoodsite/agoodsite-logo.png",
+      description: "I completed a full-time design internship at a Stockholm-based design bureau. My main assignment was to create a web-based design system for the company’s clients (mainly museums) to ensure cohesive and efficient design processes across projects.",
+      route: "/projects/agoodsite", 
       tableData: [ 
-        { label: "Technologies and Tools", value: "Mixed Reality, projection mapping, Unity, sensors, hand-tracking, physical props and Figma" },
-        { label: "My role", value: "User research and UI design" }, 
-        { label: "Duration", value: "3 months in 2022" }]
+        { label: "Users", value: "Art Directors at the design bureau (directly) and their clients (indirectly)" },
+        { label: "Methods & Tools", value: "Figma (Auto Layout, variants, style guide and more) and competitive analysis" },
+        { label: "Duration", value: "6 months in 2021" }, 
+        { label: "My role", value: "UI designer" }]
     },
   
     {
