300bcdf1ffae7143effe111e55fbcc19d7c7b66c
Stella <stella.millwood@gmail.com>
2026-02-02 17:23:15 +0100

    updated images and added accordion collapsible for tajma content

--- src/app/projects/tajma/tajma.component.ts
+++ src/app/projects/tajma/tajma.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectionStrategy,  signal } from '@angular/core';
 import { PROJECTS, Project } from '../projects-data';
 import { ProjectInfoTableComponent } from 'src/app/project-info-table/project-info-table.component';
 import { BeforeAfterComponent } from "src/app/before-after/before-after.component";
+import {MatExpansionModule} from '@angular/material/expansion';
 
 
 @Component({
@@ -9,8 +10,11 @@ import { BeforeAfterComponent } from "src/app/before-after/before-after.componen
   standalone: true,
   templateUrl: './tajma.component.html',
   styleUrl: './tajma.component.css',
-  imports: [ProjectInfoTableComponent, BeforeAfterComponent]
+  imports: [ProjectInfoTableComponent, BeforeAfterComponent, MatExpansionModule], 
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class TajmaComponent {
   project: Project = PROJECTS.find(p => p.route === "/projects/tajma")!;
+  
+  readonly panelOpenState = signal(false);
 }
